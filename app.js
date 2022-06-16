e170e9a55b6720f8b2dc3e13dd515444dbde0a7f
sajid_f57ovgd <sajidhasan2641@gmail.com>
2022-06-16 00:11:23 -0700

    added

--- app.js
+++ app.js
@@ -1,71 +1,86 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
 
 const app = express();
-var items = ["Buy Food", "Eat Food","Cook Food"]
-var workItems = []
+
 app.set('view engine', 'ejs');
- app.use(express.static( "public"));
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/toDoListDB');
+
+const itemSchema  = {
+  name : String
+};
+const Item = mongoose.model("Item",itemSchema);
+
+const Item1 = new Item({
+name : "Welcome to the toDOList"
+});
+const Item2 = new Item({
+  name : "Press +  Button to add item"
+  });
+const Item3 = new Item({
+    name : "<-- press to delete item"
+    });
+const defaultItem = [Item1,Item2, Item3];
+// Item.insertMany(defaultItem,function (err) {
+//   if (err){
+//     console.log(err);
+//   }else{
+//     console.log("Successful to log with DB");
+//   }
+// });
+
+app.get("/", function(req, res) {
+
+Item.find({},function (err, founditems) {
+  if (founditems.length === 0){
+     Item.insertMany(defaultItem,function (err) {
+  if (err){
+    console.log(err);
+  }else {
+    console.log("Successful to log with DB");
+  }
+ });
+ res.redirect("/");
+  } else {
+    res.render("list", {listTitle: "Today", newListItems: founditems});
 
 
+  }
 
-app.get("/", function (req, res) {
- let today =  new Date()
- let option = {
-     weekday : "long",
-     day : "numeric",
-     month: "long"
- }
- let day = today.toLocaleDateString("en-Us" ,option)
+});
+
  
 
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+  res.redirect("/")
  
-
-res.render("list",{listTitle : day,newlistItems :items
 });
 
-
-
-}    
-)
-app.post("/",function (req, res) {
-    let item = req.body.newitem ;
-
-    if (req.body.list === "Work"){
-        workItems.push(item) 
-        res.redirect("/work")
-
-    }else{
-        items.push(item)
-    res.redirect("/")
-    
-
-    }
-    
-    
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
-app.get("/work", function (req, res) {
-    res.render("list",{listTitle: "Work List",newlistItems: workItems});
 
-    
+app.get("/about", function(req, res){
+  res.render("about");
 });
-app.post("/work", function (req, res) {
-    let item = req.body.newitem
-    workItems.push(item)
-    res.redirect("/work")
-    
-})
 
-
-
-
-
-
-
-
-app.listen(3000, function () {
-    console.log("Server running on 3000");
-    
-})
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
